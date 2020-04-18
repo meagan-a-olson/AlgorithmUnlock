afc5992a1afc1fd2ddbda521fb7ece547e2b7e4e
Meagan Olson <meagan.olson@farmqa.com>
2020-04-17 19:40:42 -0500

    Create main panel and nest inside app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainPanelComponent } from './main-panel/main-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPanelComponent
   ],
   imports: [
     BrowserModule
