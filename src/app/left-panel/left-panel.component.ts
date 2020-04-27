a893d6acd86aeadb69431b4f81ad2f0e2a1a0d79
Meagan Olson <meagan.olson@farmqa.com>
2020-04-27 13:30:28 -0500

    Update bitcoin total and bitcoin/s every second

--- src/app/left-panel/left-panel.component.ts
+++ src/app/left-panel/left-panel.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { UpgradeService } from '../upgrade-service';
+import { UpgradeService } from './../upgrade-service';
 
 @Component({
   selector: 'app-left-panel',
@@ -13,21 +13,35 @@ export class LeftPanelComponent implements OnInit {
   constructor(public upgradeService: UpgradeService) { }
 
   ngOnInit(): void {
+    // Updates both current bitcoin total and bitcoins/s every second. Interval can be changed.
+    setInterval(() => {
+      this.updateBitcoinNum(this.bitcoinsPerSecond, true)
+      this.bitcoinsPerSecond = this.upgradeService.currentBitcoinMultiplier;
+    }, 1000)
+  }
+
+  /**
+   * Adds or subtracts the given amount of bitcoin from the running total
+   * @param {number} num - The number of bitcoin to add or subtract
+   * @param {boolean} add - True if bitcoin should be added; false if subtracted
+   */
+  updateBitcoinNum(num : number, add : boolean) {
+    this.currentNumOfBitcoins = this.upgradeService.currentNumOfBitcoins;
+    if (add) {
+      this.currentNumOfBitcoins += num;
+    } else {
+      this.currentNumOfBitcoins -= num;
+    }
+    this.upgradeService.currentNumOfBitcoins = this.currentNumOfBitcoins;
   }
 
   onClick() {
     // Method needs to run after every purchase, but before next click [Keep in this order]
-    this.currentNumOfBitcoins = this.upgradeService.currentNumOfBitcoins;
+    this.updateBitcoinNum(1, true);
+    }
 
-    this.currentNumOfBitcoins++;
-    this.upgradeService.currentNumOfBitcoins = this.currentNumOfBitcoins;
-    
-    // Method need to after every purchase, but before next click
-    this.bitcoinsPerSecond = this.upgradeService.currentBitcoinMultiplier;
-  }
   onDebugClick() {
-    this.currentNumOfBitcoins = this.upgradeService.currentNumOfBitcoins;
-    this.currentNumOfBitcoins = this.currentNumOfBitcoins + 1000000000000;
-    this.upgradeService.currentNumOfBitcoins = this.currentNumOfBitcoins;
+    this.updateBitcoinNum(1000000000000, true);
   }
+
 }
