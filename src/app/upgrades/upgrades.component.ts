c6cb9ac6ceda0da1a02000968fa88d8b8689e2c1
meagan-a-olson <58188489+meagan-a-olson@users.noreply.github.com>
2020-04-28 13:28:28 -0500

    Delete components files and disable buttons (#9)
    Co-authored-by: Meagan Olson <meagan.olson@farmqa.com>

--- src/app/upgrades/upgrades.component.ts
+++ src/app/upgrades/upgrades.component.ts
@@ -11,10 +11,18 @@ import { UpgradeService } from '../upgrade-service';
 export class UpgradesComponent implements OnInit {
 
   @Input() upgrade: Upgrade;
+  notAvailable : boolean = true;
   
   constructor(public upgradeService: UpgradeService) { }
 
   ngOnInit(): void {
+    setInterval(() => {
+      if (this.upgradeService.currentNumOfBitcoins >= this.upgrade.price) {
+        this.notAvailable = false;
+      } else {
+        this.notAvailable = true;
+      }
+    }, 500);
   }
 
   onPurchase() {
@@ -24,6 +32,5 @@ export class UpgradesComponent implements OnInit {
       this.upgradeService.currentNumOfBitcoins -= this.upgrade.price;
       this.upgradeService.deleteUpgrade(this.upgrade.name);
     }
-    
   }
 }
